d0adf4ca691d70272cea7b305b7bf6b53c5adaad
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-03-19 14:34:42 +0100

    Remove the need to define ROOT_DIR

--- test/integration-tests/run_integration_tests.sh
+++ test/integration-tests/run_integration_tests.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-ROOT_DIR="${1}"
-export TMUXCONF="${ROOT_DIR}/tmux.conf"
+export TMUXCONF="$PWD/tmux.conf"
 
 # Create a minimal tmux conf file
 > "${TMUXCONF}"
-echo TMUX_GITBAR_DIR=\"$ROOT_DIR\" >> "${TMUXCONF}"
-echo source-file \"$ROOT_DIR/tmux-gitbar.tmux\" >> "${TMUXCONF}"
+echo TMUX_GITBAR_DIR=\"$PWD\" >> "${TMUXCONF}"
+echo source-file \"$PWD/tmux-gitbar.tmux\" >> "${TMUXCONF}"
 echo set-option -g status-right \"[out of working tree]\" >> "${TMUXCONF}"
 
 bats test/integration-tests/*.bats
