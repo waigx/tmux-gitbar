02363a8edfbef682dc808b629f3db780cffd51e3
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-03-19 14:39:28 +0100

    There's just main.bats so no need for globbing

--- test/integration-tests/run_integration_tests.sh
+++ test/integration-tests/run_integration_tests.sh
@@ -8,4 +8,4 @@ echo TMUX_GITBAR_DIR=\"$PWD\" >> "${TMUXCONF}"
 echo source-file \"$PWD/tmux-gitbar.tmux\" >> "${TMUXCONF}"
 echo set-option -g status-right \"[out of working tree]\" >> "${TMUXCONF}"
 
-bats test/integration-tests/*.bats
+bats test/integration-tests/main.bats
