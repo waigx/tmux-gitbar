f1d9dfc3d8a8ec3d2c13637a7274e5cb1bfca414
Aurélien Rainone <aurelien.rainone@gmail.com>
2016-03-19 22:12:59 +0100

    Cosmetics

--- test/integration-tests/tmux-gitbar_location.tcl
+++ test/integration-tests/tmux-gitbar_location.tcl
@@ -17,22 +17,30 @@ sleep 1
 # Test gitbar location when inside a git working tree
 if {$tmgb_loc == {right}} {
 
-  assert_on_screen_regex {left.*origin/master} "branch name should show on the right"
+  assert_on_screen_regex \
+    {left.*origin/master} \
+    "branch name should show on the right"
 
 } elseif {$tmgb_loc == {left}} {
 
-  assert_on_screen_regex {origin/master.*right} "branch name should show on the left"
+  assert_on_screen_regex \
+    {origin/master.*right} \
+    "branch name should show on the left"
 }
 
 # Test gitbar location outside of git working tree
 send_cd /
 if {$tmgb_loc == {right}} {
 
-  assert_on_screen_regex {left.*out of working tree} "out of repo status string should show on the right"
+  assert_on_screen_regex \
+    {left.*out of working tree} \
+    "out of repo status string should show on the right"
 
 } elseif {$tmgb_loc == {left}} {
 
-  assert_on_screen_regex {out of working tree.*right} "out of repo status string should show on the left"
+  assert_on_screen_regex \
+    {out of working tree.*right} \
+    "out of repo status string should show on the left"
 }
 
 # End of test: success!
